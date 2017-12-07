0b341f3aa020f8cb926f3edf43f5a1b7b535938c
Kmetic <simon.kmetic@stps-trbovlje.si>
2017-12-07 13:40:42 +0100

    added errorMessages

---
+++ src/exceptions/error-messages.ts
@@ -0,0 +1,7 @@
+export class ErrorMessages {
+  static MISSING_PARAMS = 'Missing required parameters.';
+  static INVALID_PARAMS = 'Parameter of invalide type.';
+  static PARAMETER_SHOULD_BE_NUMBER = 'Parameter should be a number.';
+
+  static HAIR_SALON_NOT_FOUND = 'No hairSalon found with ID.';
+}
